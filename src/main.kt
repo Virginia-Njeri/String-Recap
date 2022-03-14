4e517cf53f5d605f9075ffbe37872c60cd3362fc
virginia-njeri <virginiaanjeri@gmail.com>
2022-03-14 22:00:39 +0300

    second commit

---
+++ src/main.kt
@@ -0,0 +1,17 @@
+fun main() {
+var T=getTown("Dodoma")
+    println(T)
+   println (getCall("wanda"))
+}
+fun getTown(town:String):String{
+    val T=town
+    return T
+}
+fun getCall(jina:String){
+    if (jina=="virginia"){
+        println("thats me!")
+
+    }else{
+     println("i dont know who that is")
+    }
+}
